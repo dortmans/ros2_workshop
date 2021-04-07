e4c50725561bc89600401cf9f7b9e1ba57a5d1bb
Eric Dortmans <eric.dortmans@gmail.com>
2021-04-07 10:51:13 +0200

    docker build improved

--- docker/build_ros2.sh
+++ docker/build_ros2.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 IMAGE_NAME=dortmans/ros2
+BASE_IMAGE=ros:foxy
 DOCKER_USER=ros2
 
+docker pull ${BASE_IMAGE}
+
 docker build \
+	--build-arg BASE_IMAGE=${BASE_IMAGE} \
 	--build-arg USER=${DOCKER_USER} \
 	--build-arg UID=$(id -u ${USER}) \
 	--build-arg GID=$(id -g ${USER}) \
 	-t ${IMAGE_NAME} .
+	
+docker image prune -f
